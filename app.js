07bf3828db6c45116c133619ca4557c707f40715
Baiyang <boyang96@yahoo.ca>
2024-04-14 04:07:12 -0400

    finish animation
    adding javascript
    finish animation wrapper

---
+++ app.js
@@ -0,0 +1,20 @@
+// let hero = document.querySelector(".hero");
+// let slider = document.querySelector(".slider");
+// let animation = document.querySelector("section.animation-wrapper");
+
+// const time_line = new TimelineMax();
+
+// //parameter1 指明哪些元素会被动画影响
+// //parameter2 是duration
+// //parameter3 包含动画开始时元素的CSS属性及其值
+// //parameter4 包含动画结束时元素的CSS属性及其值
+// time_line
+// .fromTo(hero, 1 , {height: "0%"}, {height: "100%", ease: Power2.easeInOut})
+// .fromTo(hero, 1.2 , {width: "80%"}, {width: "100%", ease: Power2.easeInOut})
+// .fromTo(slider, 1, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.2")
+// .fromTo(animation, 0.3, {opacity: 1}, {opacity: 0});
+
+// setTimeout(() => {
+//     animation.style.pointerEvents = "none";
+// }, 2500);
+
